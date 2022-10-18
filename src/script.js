39586404fbd93ec0c6409e49119c919543fa5f07
hmariejoyce <114701543+hmariejoyce@users.noreply.github.com>
2022-10-18 16:41:42 -0400

    Fixed celsius and fahrenheit links

--- src/script.js
+++ src/script.js
@@ -50,9 +50,6 @@ function search(city) {
   axios.get(weatherUrl).then(currentWeather);
 }
 
-let searchCity = document.querySelector("form");
-searchCity.addEventListener("submit", newCity);
-
 function displayCurrentWeather(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getLocation);
@@ -68,7 +65,10 @@ function currentWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#mainicon");
-  maintemp.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+  fahrenheitTemperature = response.data.main.temp;
+
+  maintemp.innerHTML = Math.round(fahrenheitTemperature);
   maincity.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -87,3 +87,29 @@ function getLocation(position) {
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
   axios.get(weatherUrl).then(currentWeather);
 }
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let maintemp = document.querySelector("#maintemp");
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+  maintemp.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let maintemp = document.querySelector("#maintemp");
+  maintemp.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitTemperature = null;
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let searchCity = document.querySelector("form");
+searchCity.addEventListener("submit", newCity);
+
+search("Virginia Beach");
