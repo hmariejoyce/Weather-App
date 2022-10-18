eaad3a01a4937d4a6049b22ba87ee80fa6e3f421
hmariejoyce <114701543+hmariejoyce@users.noreply.github.com>
2022-10-17 23:41:29 -0400

    Added Search City JS

--- src/script.js
+++ src/script.js
@@ -35,3 +35,44 @@ let hours = now.getHours() % 12 || 12;
 let ampm = now.getHours() >= 12 ? "PM" : "AM";
 
 h4.innerHTML = `${day}, ${month} ${date} ${hours}:${minutes} ${ampm}`;
+
+function newCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchcity-text-input");
+  let maincity = document.querySelector("#maincity");
+  maincity.innerHTML = `${searchInput.value}`;
+
+  search(searchInput.value);
+}
+
+function search(city) {
+  let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(weatherUrl).then(currentWeather);
+}
+
+let searchCity = document.querySelector("form");
+searchCity.addEventListener("submit", newCity);
+
+function displayCurrentWeather(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+}
+
+let getCurrentWeather = document.querySelector("#currentlocation");
+getCurrentWeather.addEventListener("click", displayCurrentWeather);
+
+function currentWeather(response) {
+  let maintemp = document.querySelector("#maintemp");
+  maintemp.innerHTML = `${Math.round(response.data.main.temp)}`;
+  let maincity = document.querySelector("#maincity");
+  maincity.innerHTML = response.data.name;
+}
+
+function getLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  axios.get(weatherUrl).then(currentWeather);
+}
